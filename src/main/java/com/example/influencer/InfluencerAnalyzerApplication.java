b292b5470b36b39243f2809e61e7e82d16a1c798
MrBitasmblTester2 <lulu2214@proton.me>
2025-11-19 11:49:39 +0400

    Add InfluencerAnalyzerApplication.java

---
+++ src/main/java/com/example/influencer/InfluencerAnalyzerApplication.java
@@ -0,0 +1,9 @@
+package com.example.influencer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class InfluencerAnalyzerApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(InfluencerAnalyzerApplication.class, args);
+  }
+} 
